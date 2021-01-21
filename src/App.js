497f7dedf64811693000c8cadda59ee11ed2a31d
TSKwon <sange17@naver.com>
2021-01-21 23:38:09 +0900

    Add create function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import TOC from "./components/TOC";
-import Content from "./components/Content"
-import Subject from "./components/Subject"
+import ReadContent from "./components/ReadContent";
+import CreateContent from "./components/CreateContent";
+import Subject from "./components/Subject";
+import Control from "./components/Control";
 import './App.css';
 
 class App extends Component {
   constructor(props){
     super(props);
+    this.max_content_id = 3;
     this.state = {
-      mode: 'read',
+      mode: 'create',
+      selected_content_id: 2,
       subject:{title: 'WEB', sub: 'World Wide Web!'},
       welcome:{title:'Welcome', desc:'Hello, React!!'},
       contents:[
@@ -19,33 +23,61 @@ class App extends Component {
     }
   }
   render() {
-    var _title, _desc = null;
+    var _title, _desc, _article = null;
     if(this.state.mode === 'welcome'){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else if(this.state.mode === 'read'){
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+    } else if(this.state.mode === 'create') {
+      _article = <CreateContent onSubmit={function(_title, _desc) {
+        // add content to this.state.contents
+        this.max_content_id = this.max_content_id + 1;
+        // this.state.contents.push({id:this.max_content_id, title: _title, desc: _desc});
+        var _contents = this.state.contents.concat(
+          {id:this.max_content_id, title: _title, desc: _desc}
+        )
+        this.setState({
+          contents:_contents
+        });
+        console.log(_title, _desc);
+      }.bind(this)}></CreateContent>
     }
     return (
       <div className="App">
-        {/* <Subject 
+        <Subject 
         title={this.state.subject.title} 
-        sub={this.state.subject.sub}>
-        </Subject> */}
-        <header>
-          <h1><a href="/" onClick={function(e){
-            console.log(e);
-            e.preventDefault();
-            // this.state.mode = 'welcome';
-            this.setState({
-              mode:'welcome'
-            });
-          }.bind(this)}>{this.state.subject.title}</a></h1>
-          {this.state.subject.sub}
-        </header>
-        <TOC data={this.state.contents}></TOC>
-        <Content title={_title} desc={_desc}></Content>
+        sub={this.state.subject.sub}
+          onChangePage={function(){
+            this.setState({mode:'welcome'});
+          }.bind(this)}
+        >
+        </Subject>
+        <TOC onChangePage={function(id){
+          this.setState({
+            mode: 'read',
+            selected_content_id:Number(id)
+          });
+        }.bind(this)}
+        data={this.state.contents}
+        ></TOC>
+        <Control onChangeMode={function(_mode){
+          this.setState({
+            mode:_mode
+          })
+        }.bind(this)}></Control>
+        {_article}
       </div>
     );
   }
